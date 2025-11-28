91b8e883bd74cf525b76fa5b4fb932344bf3a155
fliderman01 <fliderman01@gmail.com>
2025-11-28 13:31:58 +0400

    refactor: swagger docs

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,31 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import 'dotenv/config';
+import { join } from 'path';
+import { readFileSync } from 'fs';
+import { load } from 'js-yaml';
+import * as swaggerUi from 'swagger-ui-express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const yamlPath = join(process.cwd(), 'doc', 'api.yaml');
+  const yamlFile = readFileSync(yamlPath, 'utf8');
+  const document = load(yamlFile);
 
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
       transform: true,
+    }),
+  );
+
+  app.use(
+    '/doc',
+    swaggerUi.serve,
+    swaggerUi.setup(document, {
+      customSiteTitle: 'API Documentation',
     }),
   );
 
