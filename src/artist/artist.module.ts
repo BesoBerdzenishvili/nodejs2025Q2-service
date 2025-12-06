c96c715d11716cf0823ea60a4faba64b67cf04e8
fliderman01 <fliderman01@gmail.com>
2025-12-06 12:02:34 +0400

    refactor: implement  typeORM in artist module

--- src/artist/artist.module.ts
+++ src/artist/artist.module.ts
@@ -1,5 +1,7 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { ArtistController } from './artist.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Artist } from './entities/artist.entity';
 import { ArtistService } from './artist.service';
 import { AlbumModule } from '../album/album.module';
 import { TrackModule } from '../track/track.module';
@@ -7,6 +9,7 @@ import { FavoritesModule } from '../favorites/favorites.module';
 
 @Module({
   imports: [
+    TypeOrmModule.forFeature([Artist]),
     forwardRef(() => AlbumModule),
     forwardRef(() => TrackModule),
     forwardRef(() => FavoritesModule),
--- src/artist/artist.service.ts
+++ src/artist/artist.service.ts
@@ -1,4 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { randomUUID } from 'crypto';
 import { Artist } from './entities/artist.entity';
 import { CreateArtistDto, UpdateArtistDto } from './dto/artist.dto';
@@ -8,54 +10,54 @@ import { AlbumService } from 'src/album/album.service';
 @Injectable()
 export class ArtistService {
   constructor(
+    @InjectRepository(Artist)
+    private artistsRepository: Repository<Artist>,
     private readonly trackService: TrackService,
     private readonly albumService: AlbumService,
   ) {}
-  private artists: Artist[] = [];
 
-  findAll(): Artist[] {
-    return this.artists;
+  async findAll(): Promise<Artist[]> {
+    return this.artistsRepository.find();
   }
 
-  findOne(id: string): Artist {
-    const artist = this.artists.find((a) => a.id === id);
+  async findOne(id: string): Promise<Artist> {
+    const artist = await this.artistsRepository.findOne({ where: { id } });
     if (!artist) {
       throw new NotFoundException('Artist not found');
     }
     return artist;
   }
 
-  exists(id: string): boolean {
-    return this.artists.some((a) => a.id === id);
+  async exists(id: string): Promise<boolean> {
+    const count = await this.artistsRepository.count({ where: { id } });
+    return count > 0;
   }
 
-  create(createArtistDto: CreateArtistDto): Artist {
-    const artist: Artist = {
+  async create(createArtistDto: CreateArtistDto): Promise<Artist> {
+    const artist = this.artistsRepository.create({
       id: randomUUID(),
       name: createArtistDto.name,
       grammy: createArtistDto.grammy,
-    };
-    this.artists.push(artist);
-    return artist;
+    });
+    return this.artistsRepository.save(artist);
   }
 
-  update(id: string, updateArtistDto: UpdateArtistDto): Artist {
-    const artist = this.artists.find((a) => a.id === id);
+  async update(id: string, updateArtistDto: UpdateArtistDto): Promise<Artist> {
+    const artist = await this.artistsRepository.findOne({ where: { id } });
     if (!artist) {
       throw new NotFoundException('Artist not found');
     }
     Object.assign(artist, updateArtistDto);
-    return artist;
+    return this.artistsRepository.save(artist);
   }
 
-  remove(id: string): void {
-    const index = this.artists.findIndex((a) => a.id === id);
-    if (index === -1) {
+  async remove(id: string): Promise<void> {
+    const result = await this.artistsRepository.delete(id);
+    if (result.affected === 0) {
       throw new NotFoundException('Artist not found');
     }
-    this.artists.splice(index, 1);
 
-    this.trackService.nullifyArtistReferences(id);
-    this.albumService.nullifyArtistReferences(id);
+    await this.trackService.nullifyArtistReferences(id);
+    await this.albumService.nullifyArtistReferences(id);
   }
 }
