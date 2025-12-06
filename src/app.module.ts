efca06a028c6491ef841d156ef88e4c8e2fd7344
fliderman01 <fliderman01@gmail.com>
2025-12-06 11:56:23 +0400

    feat: typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
@@ -7,9 +9,28 @@ import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
 import { TrackModule } from './track/track.module';
 import { FavoritesModule } from './favorites/favorites.module';
+import { User } from './user/entities/user.entity';
+import { Artist } from './artist/entities/artist.entity';
+import { Album } from './album/entities/album.entity';
+import { Track } from './track/entities/track.entity';
+import { Favorite } from './favorites/entities/favorite.entity';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '5432'),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [User, Artist, Album, Track, Favorite],
+      synchronize: true,
+    }),
     UserModule,
     AuthModule,
     ArtistModule,
--- src/favorites/entities/favorite.entity.ts
+++ src/favorites/entities/favorite.entity.ts
@@ -1,6 +1,22 @@
 import { Album } from '../../album/entities/album.entity';
 import { Track } from '../../track/entities/track.entity';
 import { Artist } from '../../artist/entities/artist.entity';
+import { Entity, PrimaryColumn, Column } from 'typeorm';
+
+@Entity('favorites')
+export class Favorite {
+  @PrimaryColumn('uuid')
+  id: string;
+
+  @Column({ type: 'uuid', nullable: true })
+  artistId: string | null;
+
+  @Column({ type: 'uuid', nullable: true })
+  albumId: string | null;
+
+  @Column({ type: 'uuid', nullable: true })
+  trackId: string | null;
+}
 
 export interface Favorites {
   artists: string[];
