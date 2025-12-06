2038ee6d62b7709cdc694c503036f3b8d006f82b
fliderman01 <fliderman01@gmail.com>
2025-12-06 12:06:41 +0400

    refactor: implement typeORM in track module

--- src/track/track.module.ts
+++ src/track/track.module.ts
@@ -2,9 +2,14 @@ import { Module, forwardRef } from '@nestjs/common';
 import { TrackController } from './track.controller';
 import { TrackService } from './track.service';
 import { FavoritesModule } from '../favorites/favorites.module';
+import { Track } from './entities/track.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [forwardRef(() => FavoritesModule)],
+  imports: [
+    forwardRef(() => FavoritesModule),
+    TypeOrmModule.forFeature([Track]),
+  ],
   controllers: [TrackController],
   providers: [TrackService],
   exports: [TrackService],
